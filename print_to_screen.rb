e0679c167972aae2d6e1114995f73c9a58d32730
Jordan Endicott <jordan.endicott823@gmail.com>
2018-06-06 13:48:06 -0400

    continued working on our function

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -18,8 +18,10 @@
 number = 1
 
 100.times do
-    if number == 3
-        puts "mind"
+    if number %3 == 0
+        puts "mined"
+    elsif number %5 == 0
+        puts "minds"
     else
         puts number
     end
