26ec6cb69b2ef167b746b5b75f3a4f77ad130293
Jordan Endicott <jordan.endicott823@gmail.com>
2018-06-06 14:41:40 -0400

    put comments on each line to explain what they do

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -15,16 +15,18 @@
 # end
 
 
-number = 1
+number = 1 # sets variable "number" to 1
 
-100.times do
-    if number %3 == 0
-        puts "mined"
-    elsif number %5 == 0
-        puts "minds"
+100.times do # runs function 100 times
+if number % 15 == 0 # if the number is divisible by 3 and 5, it's replaced
+    puts "mined minds" # defines what to replace the numbers with
+    elsif number % 3 == 0 # if the number is divisible by 3, it's replaced
+        puts "mined" # defines what to replace the numbers with
+    elsif number % 5 == 0 # if the number is divisible by 5, it's replaced
+        puts "minds"# defines what to replace the numbers with
     else
-        puts number
-    end
-    number = number + 1
+        puts number #puts the number if its not changed above
+    end # ends if statement
+    number = number + 1 # ads 1 to the number variable each time the function runs
 
-end
+end # ends the function
