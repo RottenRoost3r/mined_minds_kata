d75ff1d2a158c86e940a9dde4db352281207083d
Jordan Endicott <jordan.endicott823@gmail.com>
2018-06-06 12:11:48 -0400

    first commit

---
+++ print_to_screen.rb
@@ -0,0 +1,28 @@
+# p 1
+# puts 1
+# print 1
+
+# 100.times do
+#     puts 1
+# end
+
+# number = 1
+
+# 100.times do
+#     puts number
+#     number = number + 1
+
+# end
+
+
+number = 1
+
+100.times do
+    if number == 3
+        puts "mind"
+    else
+        puts number
+    end
+    number = number + 1
+
+end
